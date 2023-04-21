b5faf6fd0688eb656990bab83d08fb3d301d1d22
Kriktunov <130924321+Kriktunov@users.noreply.github.com>
2023-04-21 14:00:10 +0300

    Выполнение Задачи 1

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,4 +2,16 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
+  
+  val arrout = Array(n, {""})
+        for ( i in  arrout.indices) {
+            if (i % 3 == 1 ) {
+                arrout[i] = "Fizz"
+            } else if (i % 5 == 1 )  arrout[i] = "Buzz"
+
+            else if (i % 3 == 1 && i % 5 == 1) arrout[i] ="FizzBuzz"
+
+            else arrout[i] = "$i"
+        }
+        return arrout
 }
